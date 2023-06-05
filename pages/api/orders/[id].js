e7b8f8ca0a1843556af8d9b769a0afa331b50a1e
daniel <daniel.enrriquesmercedes@gmail.com>
2023-06-05 14:53:22 +0200

    FT: i fixed the build error with type

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -1,12 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import {PrismaClient} from '@prisma/client'
 import products from '../../../prisma/data/products';
 const prisma = new PrismaClient();
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req, res
 ) {
 
     
